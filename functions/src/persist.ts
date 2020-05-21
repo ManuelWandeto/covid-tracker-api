9e7302d0816b07d20601aa2a2a91265f73930ab5
ManuelWandeto <immanuelmojo@hotmail.com>
2020-05-21 20:16:38 +0300

    update returned log date

--- functions/src/persist.ts
+++ functions/src/persist.ts
@@ -2,12 +2,14 @@ import { WorldwideStats } from "./interfaces";
 
 export async function persistStats(db: FirebaseFirestore.Firestore, stats: WorldwideStats) {
     try {
+        const currentDate = new Date().toUTCString();
+        
         await db.collection('Stats').doc('globalStats').set({
-            updatedAt: new Date().toLocaleString(),
+            updatedAt: currentDate,
             worldwide: stats.worldwide,
             countries: stats.countries
         })
-        return `successfully persisted stats ${new Date().getDay()}`;
+        return `successfully persisted stats ${currentDate}`;
     } catch (error) {
         return `error persisting stats ${error}`;
     }
