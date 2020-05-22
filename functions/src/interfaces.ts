aef2751da315f7d184765c184c4da60ef1b9f724
ManuelWandeto <immanuelmojo@hotmail.com>
2020-05-22 17:53:03 +0300

    merge GlobalData and CountryData to StatData with optional properties

--- functions/src/interfaces.ts
+++ functions/src/interfaces.ts
@@ -1,6 +1,6 @@
-export interface CountryData {
-    countryName: string,
-    countryCode: string,
+export interface StatData {
+    countryName?: string,
+    countryCode?: string,
     confirmed: number,
     active: number,
     recovered: number,
@@ -8,15 +8,8 @@ export interface CountryData {
     deaths: number
 }
 
-export interface GlobalData {
-    confirmed: number,
-    active: number,
-    recovered: number,
-    critical: number,
-    deaths: number
-}
 
 export interface WorldwideStats {
-    worldwide: GlobalData,
-    countries: CountryData[]
+    worldwide: StatData,
+    countries: StatData[]
 }
