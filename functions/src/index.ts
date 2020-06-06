fd7982468f24a482589b27cc90f4496d75f04226
ManuelWandeto <immanuelmojo@hotmail.com>
2020-06-06 21:34:03 +0300

    update web api logic and exported function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,49 +1,46 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
-import * as express from 'express';
-import * as cors from 'cors';
-import { scrapeGlobalStats } from './scraper';
-import { persistStats } from './persist';
+import express from 'express';
+import {GlobalStats} from './interfaces';
+import cors from 'cors';
+import getData from './organiser';
+import {readStatsFromFile, writeStatsToFile} from './utils';
 
 const serviceAccount: admin.ServiceAccount = require('../assets/firebasePermissions.json');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL:
-        "https://covid-tracker-api-c2a95.firebaseio.com",
+    storageBucket: 'covid-tracker-api-c2a95.appspot.com/'
 })
 
 const app = express();
-const db = admin.firestore();
 const corsOptions: cors.CorsOptions = {
     origin: true
 }
+const bucket = admin.storage().bucket();
+const file = bucket.file('Data/stats.json');
 
 app.use(cors(corsOptions))
 
 app.get('/globalStats', async (req, res) => {
-    const docRef = db.collection("Stats").doc("globalStats");
-    docRef.get().then((doc) => {
-        if (doc.exists) {
-            res.send(JSON.stringify(doc.data()));
-        } else {
-            res.statusMessage = "document not found";
-            res
-                .status(404)
-                .end()
-        }
+    readStatsFromFile<GlobalStats>(file).then(stats => {
+        res.status(200).send(JSON.stringify(stats));
     }).catch(err => {
-        res.statusMessage = "internal server error";
-        res.status(500).end()
-    });
+        res.statusMessage = `server error occured retrieving stats: ${err}`;
+        res.status(500).end();
+    })
 });
 
-exports.api = functions.runWith({memory: "256MB", timeoutSeconds: 25}).https.onRequest(app);
-exports.scrapeStats = functions
-        .runWith({memory: "1GB", timeoutSeconds: 45})
-        .pubsub.schedule("every 60 minutes")
-        .onRun(async (context) => {
-            const stats = await scrapeGlobalStats();
-            const statusMsg = await persistStats(db, stats);
-            console.log(statusMsg);
-        })
+exports.api = functions.https.onRequest(app);
+exports.scraper = functions
+                    .runWith({memory: '2GB', timeoutSeconds: 90})
+                    .region('asia-east2')
+                    .pubsub.schedule('every 30 minutes')
+                    .onRun(async (context) => {
+                        getData().then(async stats => {
+                            const statusMessage = await writeStatsToFile<GlobalStats>(file, stats);
+                            console.log(statusMessage);
+                        }).catch(error => {
+                            console.log(`error scraping data: ${error}`);
+                        });
+                    });
