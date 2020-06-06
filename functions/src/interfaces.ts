333879f388a23e443c8d03a901e3e9214445bc92
ManuelWandeto <immanuelmojo@hotmail.com>
2020-06-06 21:30:31 +0300

    update interfaces for inclusive data from both scrape sources

--- functions/src/interfaces.ts
+++ functions/src/interfaces.ts
@@ -1,19 +1,32 @@
-export interface StatData {
-    countryName?: string,
-    countryCode?: string,
-    latLng?: {
+export interface StateData {
+    [index: string]: any,
+    name: string,
+    countryCode: string,
+    stateCode?: string,
+    states?: StateData[],
+    latLng: {
         latitude: number,
-        longitude: number
+        longitude: number,
     }
+    population: number,
+    tests?: number,
     confirmed: number,
     active: number,
     recovered: number,
     critical: number,
-    deaths: number
+    deaths: number,
 }
 
-
-export interface WorldwideStats {
-    worldwide: StatData,
-    countries: StatData[]
+export interface GlobalStats {
+    updatedAt: Date,
+    worldwide: {
+        tests?: number,
+        confirmed: number,
+        active: number,
+        recovered: number,
+        critical: number,
+        deaths: number
+    },
+    countries: StateData[]
 }
+
