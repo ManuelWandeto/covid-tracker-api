74425593e9b0fef58b03a626022827cfefa89039
ManuelWandeto <immanuelmojo@hotmail.com>
2020-06-08 11:32:27 +0300

    split scraper function into multiple cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,8 +3,10 @@ import * as admin from 'firebase-admin';
 import express from 'express';
 import {GlobalStats} from './interfaces';
 import cors from 'cors';
-import getData from './organiser';
+import scrapeGlobalStats, {CountryData} from './globalDataScraper';
+import scrapeRegionStats, {RegionData} from './regionScraper';
 import {readStatsFromFile, writeStatsToFile} from './utils';
+import getData from './organiser';
 
 const serviceAccount: admin.ServiceAccount = require('../assets/firebasePermissions.json');
 
@@ -31,16 +33,55 @@ app.get('/globalStats', async (req, res) => {
     })
 });
 
+
 exports.api = functions.https.onRequest(app);
-exports.scraper = functions
-                    .runWith({memory: '2GB', timeoutSeconds: 90})
-                    .region('asia-east2')
-                    .pubsub.schedule('every 30 minutes')
-                    .onRun(async (context) => {
-                        getData().then(async stats => {
-                            const statusMessage = await writeStatsToFile<GlobalStats>(file, stats);
-                            console.log(statusMessage);
-                        }).catch(error => {
-                            console.log(`error scraping data: ${error}`);
-                        });
-                    });
+exports.globalScraper = functions
+            .runWith({memory: '1GB', timeoutSeconds: 60})
+            .pubsub.schedule('every 25 minutes')
+            .onRun(async (context) => {
+                try {
+                    const globalStats = await scrapeGlobalStats();
+                    const statusMsg = await writeStatsToFile<CountryData[]>(bucket.file('Data/globalStats.json'), globalStats);
+                    console.log(statusMsg);
+                } catch (error) {
+                    console.log(`error occured getting global stats: ${error}`);
+                }
+            });
+
+exports.regionScraper = functions
+            .runWith({memory: '1GB', timeoutSeconds: 100})
+            .pubsub.schedule('every 25 minutes')
+            .onRun(async (context) => {
+                const regions = ['world', 'unitedstates', 'canada', 'australia', 'russia', 'italy'];
+                try {
+                    const regionStats = await scrapeRegionStats(regions);
+                    const statusMsg = await writeStatsToFile<RegionData[]>(bucket.file('Data/regionStats.json'), regionStats);
+                    console.log(statusMsg);
+                } catch (error) {
+                    console.log(`error getting region stats: ${error}`);
+                }
+            })
+
+exports.organiseData = functions
+            .runWith({memory: '512MB'})
+            .storage
+            .object().onFinalize(async (object) => {
+                if((object.name?.includes('stats.json'))) {
+                    console.log(`skipping execution for file ${object.name}`);
+                    return;
+                } else {
+                    try {
+                        const globalStats = await readStatsFromFile<CountryData[]>(bucket.file('Data/globalStats.json'));
+                        const regionStats = await readStatsFromFile<RegionData[]>(bucket.file('Data/regionStats.json'));
+                        if(regionStats && globalStats) {
+                            const stats = await getData(globalStats, regionStats);
+                            const statusMsg = await writeStatsToFile<GlobalStats>(file, stats);
+                            console.log(statusMsg);
+                        } else {
+                            throw new Error(`error getting data from file sources`);
+                        }
+                    } catch (error) {
+                        console.log(`error organising data into stats: ${error}`);
+                    }    
+                }
+            })
