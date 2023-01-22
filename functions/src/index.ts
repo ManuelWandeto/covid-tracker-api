efb5dbe3e3057a25627ecea95463355565214abf
Manuel Wandeto <immanuelmojo@hotmail.com>
2023-01-22 22:23:01 +0300

    Update index.ts
    - schedule separate recurrent jobs for scraping and organizing country and region data
    - remove commented firebase code
    - update route handler to get global organised stats

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,101 +3,62 @@ import cors from 'cors';
 import {GlobalStats, RegionData, CountryData} from './interfaces'
 import scrapeRegionStats from './regionScraper';
 import scrapeGlobalStats from './globalDataScraper';
-import { readStatsFromFile, writeStatsToFile } from './utils';
+import { readStatsFromFile, writeStatsToFile, olderThan } from './utils';
 import organizeData from './organiser';
+import schedule from 'node-schedule'
+
+
+schedule.scheduleJob('getGlobalCountryData', '*/15 * * * *', async () => {
+    try {
+        const countryData = await scrapeGlobalStats(60000)
+        const statusMsg = await writeStatsToFile<CountryData[]>('./data/global_country_data.json', countryData)
+        console.log(statusMsg)
+    } catch (error) {
+        console.log(`Error occured scraping countryData: ${error} at: ${new Date()}`);
+    }
+})
+
+schedule.scheduleJob('getRegionData', '*/10 * * * *', async () => {
+    const regions = ['world', 'unitedstates', 'canada', 'australia', 'russia', 'italy'];
+    try {
+        const regionData = await scrapeRegionStats(regions)
+        const statusMsg = await writeStatsToFile<RegionData[]>('./data/region_stats.json', regionData)
+        console.log(statusMsg)
+    } catch (error) {
+        console.log(`Error occured scraping regionData: ${error} at: ${new Date()}`);
+        
+    }
+})
+
+schedule.scheduleJob('organiseData', '*/20 * * * *', async () => {
+    try {
+        if(await olderThan("./data/region_stats.json", 1440) || await olderThan("./data/global_country_data.json", 1440)) {
+            console.log(`WARN: Data is getting stale`)
+        }
+        const regionStats = readStatsFromFile<RegionData[]>("./data/region_stats.json")
+        const countryStats = readStatsFromFile<CountryData[]>("./data/global_country_data.json")
+        if(!regionStats || !countryStats) throw new Error("Error getting data from file sources");
+        const organised = await organizeData(countryStats, regionStats)
+        const statusMsg = await writeStatsToFile<GlobalStats>("./data/global_stats.json", organised)
+        console.log(statusMsg)
+    } catch (error) {
+        console.log(`Error occured organizing data: ${error} at: ${new Date()}`);
+    }
+})
 
 const app = express();
 const corsOptions: cors.CorsOptions = {
     origin: true
 }
 
-const regions = ['world', 'unitedstates', 'canada', 'australia', 'russia', 'italy'];
 app.use(cors(corsOptions))
-app.get('/regionalStats', async (req, res) => {
+app.get('/getCovidStats', async (req, res) => {
     try {
-        const regionStats = await scrapeRegionStats(regions)
-        const statusMsg = await writeStatsToFile<RegionData[]>('./data/region_stats.json', regionStats)
-        console.log(statusMsg)
-        res.send(JSON.stringify(regionStats));
-    } catch (error) {
-        console.log(error);
-        res.status(500).send(JSON.stringify(error))
-    }
-});
-app.get("/globalStats", async (req, res) => {
-    try {
-        const globalStats = await scrapeGlobalStats()
-        const statusMsg = await writeStatsToFile<CountryData[]>('./data/global_country_data.json', globalStats)
-        console.log(statusMsg)
+        const globalStats = readStatsFromFile<GlobalStats[]>("./data/global_stats.json")
         res.send(JSON.stringify(globalStats));
     } catch (error) {
-        console.log(error);
-        res.status(500).send(JSON.stringify(error))
-    }
-})
-app.get('/organised', async (req, res) => {
-    try {
-        const regionStats = readStatsFromFile<RegionData[]>("./data/region_stats.json")
-        const countryStats = readStatsFromFile<CountryData[]>("./data/global_country_data.json")
-        const organised = await organizeData(countryStats, regionStats)
-        const statusMsg = await writeStatsToFile<GlobalStats>("./data/global_stats.json", organised)
-        console.log(statusMsg)
-        res.send(JSON.stringify(organised));
-    } catch (error) {
-        console.log(error);
+        res.statusMessage = `Error occured getting global stats: ${error}`
         res.status(500).send(error)
     }
 })
 app.listen(3000, () => console.log('server started'));
-
-// exports.api = functions.https.onRequest(app);
-// exports.globalScraper = functions
-//             .runWith({memory: '1GB', timeoutSeconds: 60})
-//             .pubsub.schedule('every 25 minutes')
-//             .onRun(async (context) => {
-//                 try {
-//                     const globalStats = await scrapeGlobalStats();
-//                     const statusMsg = await writeStatsToFile<CountryData[]>(bucket.file('Data/globalStats.json'), globalStats);
-//                     console.log(statusMsg);
-//                 } catch (error) {
-//                     console.log(`error occured getting global stats: ${error}`);
-//                 }
-//             });
-
-// exports.regionScraper = functions
-//             .runWith({memory: '1GB', timeoutSeconds: 100})
-//             .pubsub.schedule('every 25 minutes')
-//             .onRun(async (context) => {
-//                 const regions = ['world', 'unitedstates', 'canada', 'australia', 'russia', 'italy'];
-//                 try {
-//                     const regionStats = await scrapeRegionStats(regions);
-//                     const statusMsg = await writeStatsToFile<RegionData[]>(bucket.file('Data/regionStats.json'), regionStats);
-//                     console.log(statusMsg);
-//                 } catch (error) {
-//                     console.log(`error getting region stats: ${error}`);
-//                 }
-//             })
-
-// exports.organiseData = functions
-//             .runWith({memory: '512MB'})
-//             .storage
-//             .object().onFinalize(async (object) => {
-//                 if((object.name?.includes('stats.json'))) {
-//                     console.log(`skipping execution for file ${object.name}`);
-//                     return;
-//                 } else {
-//                     try {
-//                         const globalStats = await readStatsFromFile<CountryData[]>(bucket.file('Data/globalStats.json'));
-//                         const regionStats = await readStatsFromFile<RegionData[]>(bucket.file('Data/regionStats.json'));
-//                         if(regionStats && globalStats) {
-//                             const stats = await getData(globalStats, regionStats);
-//                             const statusMsg = await writeStatsToFile<GlobalStats>(file, stats);
-//                             console.log(statusMsg);
-//                         } else {
-//                             throw new Error(`error getting data from file sources`);
-//                         }
-//                     } catch (error) {
-//                         console.log(`error organising data into stats: ${error}`);
-//                     }    
-//                 }
-//             })
